7ff0291535c79d0a475aa3aae2ec3dc7c623e8fc
DEJAN\38163 <cola_dcx@yahoo.com>
2023-06-12 01:53:14 +0200

    final touches

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -31,7 +31,7 @@
         <h2>All posts</h2>
         @foreach ($posts as $post)
             <div style="background: rgb(239, 241, 217); padding: 10px; margin:10px">
-                <h3>{{$post['title']}}</h3>
+                <h3>{{$post['title']}} by {{$post->user->name}}</h3>
                 {{$post['body']}}
                 <p><a href="/edit-post/{{$post->id}}">Edit</a></p>
                 <form action="/delete-post/{{$post->id}}" method="POST">
